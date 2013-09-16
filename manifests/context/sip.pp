3fb07c16e7bb1d95a63ae3fb1e6402c6b5c911ae
Maximilian Ronniger <mxr@rise-world.com>
2013-09-16 12:51:21 +0200

    Fixed Tab allignment for parameters.

--- manifests/context/sip.pp
+++ manifests/context/sip.pp
@@ -1,35 +1,29 @@
 define asterisk::context::sip (
-  $ensure = 'present',
-
-  $username = true,
-  $defaultuser = false,
+  $ensure        = 'present',
+  $username      = true,
+  $defaultuser   = false,
   $template_name = false,
-
-  $secret  = false,
-  $context = false,
-
-  $account_type = 'friend',
-  $canreinvite = 'no',
-  $host = 'dynamic',
-  $insecure = 'no',
-  $language = 'en',
-  $nat = false,
-  $qualify = no,
-  $vmexten = false,
-
-  $callerid = false,
-  $calllimit = false,
-  $callgroup = false,
-  $mailbox = false,
-  $md5secret = false,
-  $pickupgroup = false,
-  
-  $fromdomain = false,
+  $secret        = false,
+  $context       = false,
+  $account_type  = 'friend',
+  $canreinvite   = 'no',
+  $host          = 'dynamic',
+  $insecure      = 'no',
+  $language      = 'en',
+  $nat           = false,
+  $qualify       = no,
+  $vmexten       = false,
+  $callerid      = false,
+  $calllimit     = false,
+  $callgroup     = false,
+  $mailbox       = false,
+  $md5secret     = false,
+  $pickupgroup   = false,
+  $fromdomain    = false,
   $outboundproxy = false,
-
-  $disallow = [],
-  $allow = [],
-  $dtmfmode = false) {
+  $disallow      = [],
+  $allow         = [],
+  $dtmfmode      = false) {
   require asterisk::sip
 
   asterisk::dotd_file {"sip_${name}.conf":

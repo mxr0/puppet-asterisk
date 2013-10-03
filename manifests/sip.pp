7b06d60411f6e43a93850608f222a6109897f4c8
Maximilian Ronniger <mxr@rise-world.com>
2013-10-03 14:51:24 +0200

    syntax error in last commit.

--- manifests/sip.pp
+++ manifests/sip.pp
@@ -3,10 +3,10 @@ class asterisk::sip (
 $sip_options = $asterisk::params::sip_options,
 ) inherits asterisk::params{
 
-  validate_array($sip_options.['allow'])
-  validate_array($sip_options.['disallow'])
-  validate_array($sip_options.['domain'])
-  validate_array($sip_options.['localnet'])
+  validate_array($sip_options['allow'])
+  validate_array($sip_options['disallow'])
+  validate_array($sip_options['domain'])
+  validate_array($sip_options['localnet'])
   
   asterisk::config_dotd {'/etc/asterisk/sip.conf':
       additional_paths => ['/etc/asterisk/sip.registry.d'],
